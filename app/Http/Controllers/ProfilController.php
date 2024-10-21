6c2bb00271a90effc921ce4c3bc0c5e0a3ef401a
ssophii <ssophinaa@gmail.com>
2024-10-21 19:17:43 +0700

    report.4

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -54,9 +54,6 @@ class ProfilController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Profil $profil)   
     {
         return view(
@@ -65,29 +62,14 @@ class ProfilController extends Controller
         );
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        $validated = $request->validate([
-            // 'judul' => 'required',
-            // 'penulis' => 'required',
-            // 'penerbit' => 'required',
-            // 'sinopsis' => 'required',
-            // 'tahun' => 'required',
-            // 'stok' => 'required',
-            // 'status' => 'required',
-        ]);
-        $buku = Profil::find($id);
-        // dd($buku);
-        $buku->update($request->all()); //
-        return redirect()->route('profil')->with('success', 'Data buku berhasil diubah');
+        $validated = $request->validate([]);
+        $profil = Profil::find($id);
+        $profil->update($request->all()); //
+        return redirect()->route('profil')->with('success', 'Data profil berhasil diubah');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Profil $profil)
     {
         $profil->delete();
